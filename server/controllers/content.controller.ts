461c4c4b5055a4b6902551a1c1d35451396bc2f7
7TIN <prasadgujarathi619@gmail.com>
2025-09-29 02:41:47 +0530

    feat : Content Controller completed

--- server/controllers/content.controller.ts
+++ server/controllers/content.controller.ts
@@ -1,11 +1,19 @@
 
-
-// todo 1. add content 2. delete content by id 3. get all contents 4. get content by id 
-
 import type { NextFunction, Request, Response } from "express";
 import Content from "../models/content.model";
-import { resolveSync } from "bun";
+import { z } from "zod";
 
+const contentTypes = ['image', 'video', 'article', 'audio'] as const;
+
+export const zodContentSchema = z.object({
+  link: z.string().min(1, "Link is required"),
+  title: z.string().min(1, "Title is required"),
+  useId: z.string().regex(/^[a-fA-F0-9]{24}$/, "Invalid ObjectId"), // MongoDB ObjectId format
+  type: z.enum(contentTypes),
+  tags: z.array(z.string()).optional()
+});
+
+export type ContentInput = z.infer<typeof zodContentSchema>;
 
 export const addContent = async(req : Request, res : Response, next: NextFunction ) => {
 
@@ -21,7 +29,6 @@ export const addContent = async(req : Request, res : Response, next: NextFunctio
         next(error);
     }
 }
-
 
 export const getContents = async(req : Request, res : Response, next: NextFunction ) => {
     try {
@@ -40,6 +47,37 @@ export const getContentsById = async (req : Request, res : Response, next: NextF
         res.status(200).json({
             Content: foundContent,
         })
+    }catch (error){
+        next(error)
+    }
+}
+
+export const updateContent = async(req : Request, res : Response, next: NextFunction ) => {
+    try {
+        const data = zodContentSchema.parse(req.body); 
+        const updateData = await Content.findByIdAndUpdate(req.params.id, data, {new: true})
+
+        if(!updateData){
+            res.status(404).json({message: "Content not found to update"})
+        }
+
+        res.status(200).json({
+            Content : updateData,
+        })
+    }catch (error) {
+        next(error)
+    }
+}
+
+export const deleteContent = async(req : Request, res : Response, next: NextFunction ) => {
+    try {
+        const deleteFoundContent = await Content.findByIdAndDelete(req.params.id);
+
+        if(!deleteFoundContent){
+            res.status(404).json({message : "Content not fount to delete"})
+        }
+        res.status(300).send("Deleted Successfully");
+
     }catch (error){
         next(error)
     }
--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -1,16 +1,18 @@
-import type { NextFunction, Request, Response } from "express";
-import mongoose from "mongoose";
+// import type { NextFunction, Request, Response } from "express";
+// import mongoose from "mongoose";
 
 
-export const AddUser = async(req: Request, res: Response , next: NextFunction ) =>{
+// export const AddUser = async(req: Request, res: Response , next: NextFunction ) => {
 
-    // const session = await mongoose.startSession();
 
-    // session.startTransaction();
 
-    // await session.commitTransaction();
-    // await session.endSession();
+//     // const session = await mongoose.startSession();
+
+//     // session.startTransaction();
+
+//     // await session.commitTransaction();
+//     // await session.endSession();
     
 
 
-}
+// }
