f169e0fee3cf8dd85705c3581e0ef67facdd9cc9
7TIN <prasadgujarathi619@gmail.com>
2025-10-25 01:16:41 +0530

    feat: embedded card conditional rendering by links

--- client/src/components/EmbeddedCard.tsx
+++ client/src/components/EmbeddedCard.tsx
@@ -1,20 +1,46 @@
 // import React from 'react'
-// import { XEmbed } from 'react-social-media-embed'
-import { YouTubeEmbed } from 'react-social-media-embed'
-import { cn } from '../lib/utils'
+import { XEmbed } from "react-social-media-embed";
+import { YouTubeEmbed } from "react-social-media-embed";
+import { cn } from "../lib/utils";
 
 type EmbeddedCardProps = {
-    link: string
-    className?: string
-}
+  link: string;
+  className?: string;
+};
 
-export const EmbeddedCard = ({link, className}:EmbeddedCardProps) => {
+export const EmbeddedCard = ({ link, className }: EmbeddedCardProps) => {
+  const Link = link.split("://")[1].split("/")[0];
   return (
-        // <div className={cn('bg-white p-px rounded-md overflow-visible antialiased will-change-transform',className)}>
-        //   <XEmbed url={link} />
-        // </div>
-        <div className={cn('bg-white border border-slate-300 py-1 overflow-visible antialiased will-change-transform flex justify-center items-center',className)}>
-          <YouTubeEmbed className='bg-white' height={200} width={312} url={link}/>
+    <>
+      {(Link === "youtu.be" && (
+        <div
+          className={cn(
+            "bg-white border border-slate-300 py-1 overflow-visible antialiased will-change-transform flex justify-center items-center",
+            className
+          )}
+        >
+          <YouTubeEmbed
+            className="bg-white"
+            height={200}
+            width={312}
+            url={link}
+          />
         </div>
-  )
-}
+      )) ||
+        (Link === "x.com" && (
+          <div
+            className={cn(
+              "bg-white p-px rounded-md overflow-visible antialiased will-change-transform",
+              className
+            )}
+          >
+            <XEmbed url={link} />
+          </div>
+        )) || (Link === "twitter.com" && (
+          <div className={cn("bg-white p-px rounded-md overflow-visible antialiased will-change-transform",className)}>
+            <XEmbed url={link.replace("twitter.com","x.com")} />
+          </div>
+        )) }
+    </>
+  );
+};
